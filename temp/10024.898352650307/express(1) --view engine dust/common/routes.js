ae791be79496d8cd6b7da82a6a73d105c5475ae1
Blu Capital <bcapital@localhost.localdomain>
2018-10-02 15:28:17 -0600

    gitignore

--- temp/10024.898352650307/express(1) --view engine dust/common/routes.js
+++
@@ -1,8 +0,0 @@
-const Home = require('../app/controllers/Home');
-//you can include all your controllers
-
-module.exports = function (app) {
-
-		app.get('/', (req, res) => { Home.index(req, res) } );
-
-}
